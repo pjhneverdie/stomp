8d33e31e9a9e253617fce0967562929c82dd7c1c
JINHYEOK PARK <pjhneverdie@gmail.com>
2026-02-19 11:52:02 +0000

    OAuth2 세팅 중

--- stomp/src/main/java/com/example/stomp/StompApplication.java
+++ stomp/src/main/java/com/example/stomp/StompApplication.java
@@ -2,6 +2,7 @@ package com.example.stomp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientPropertiesMapper;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 import com.redis.om.spring.annotations.EnableRedisDocumentRepositories;
@@ -13,6 +14,12 @@ public class StompApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StompApplication.class, args);
+
+// OAuth2ClientProperties 
+		new OAuth2ClientPropertiesMapper()
 	}
+
+	
+
 
 }
